b81f8257ef12da37e1a4b2941a8836d52ca8529f
Nicholas Houle <181gaming@gmail.com>
2018-11-30 16:44:24 -0700

    Update project notes.

--- unbound/start.sh
+++ unbound/start.sh
@@ -3,13 +3,14 @@ set -e
 
 # Variables
 declare -g d_ip_v4
+declare -g d_dns_host='cloudflare-dns.com'
 d_ip_v4=$(hostname -I)
 
 # Ensure unbound setup
 /usr/sbin/unbound-control-setup
 
 # Information
-printf "Unbound DNS running
+printf "Unbound DNS cloudflare-dns.com
 Point your DNS to %s and pass through unbound.
 
 DNS (IPv4): %s\n" "${d_ip_v4}" "${d_ip_v4}"
