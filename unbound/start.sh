11ac6811769a91c33b3159efa69e7133221ab999
Nicholas Houle <181gaming@gmail.com>
2018-11-29 18:04:17 -0700

    My first docker project.

---
+++ unbound/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+/usr/sbin/unbound-control-setup
+/usr/sbin/unbound-control start > /dev/null 2>&1
+
+printf "
+Unbound DNS running
+Point your DNS to %s and pass through unbound.
+
+DNS: %s" "$(hostname -I)" "$(hostname -I)" && tail -f /dev/null
