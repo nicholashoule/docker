d6b444a4d8e5b4b525fd364f889e1c569bad7dfd
Nicholas Houle <181gaming@gmail.com>
2018-12-03 18:05:25 -0700

    Start versioning unbound docker container.

---
+++ unbound/1.6.7/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Variables
+declare -l d_ip_v4 d_dns_host d_unbound_v
+d_ip_v4=$(hostname -I)
+d_dns_host='cloudflare-dns.com'
+d_unbound_v='1.6.7'
+
+# Ensure unbound setup
+/usr/sbin/unbound-control-setup
+
+# Information
+printf "\nUnbound %s DNS %s
+Point your DNS to %s and pass through unbound.
+
+DNS (IPv4): %s\n" "${d_unbound_v}" "${d_dns_host}" "${d_ip_v4}" "${d_ip_v4}"
+
+# Start the service
+#/usr/sbin/unbound -c /etc/unbound/unbound.conf -d -v
+#/usr/sbin/unbound -h
+/usr/sbin/service unbound start && tail -f /dev/null
